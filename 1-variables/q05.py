d106d937edf87a832d80ea1090340af087105454
lokeshgiripunje <lokeshgiripunje@gmail.com>
2025-07-11 20:03:44 +0530

    Solved q05 of python assignmnet

--- 1-variables/q05.py
+++ 1-variables/q05.py
@@ -6,8 +6,10 @@ previous_location = 'UK'
 current_location = 'India'
 print(f'I was in {previous_location} and moved back to {current_location} 2 years ago.')
 
-# Swap previus and curren location using temporary variable temp_variable and print same print statement.
-----
-----
-----
-print(----'I was in {previous_location} and moved back to {current_location} 2 years ago.')
+
+# Temporary variable
+temp_variable = previous_location
+previous_location = current_location
+current_location = temp_variable
+# Swap previous and current location using temporary variable temp_variable and print same print statement.
+print(f'I was in {previous_location} and moved back to {current_location} 2 years ago.')
